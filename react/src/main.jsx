4a73798892404b34ebe730d10ba23f2394ed8554
prajwal <acharyaprajwal969@gmail.com>
2023-08-30 22:55:18 +0530

    prajwal

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -2,13 +2,14 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import {NextUIProvider} from "@nextui-org/react"
-import {BrowserProvider} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
   <NextUIProvider>
-    <BrowserProvider>
       <App/>
-    </BrowserProvider>
   </NextUIProvider>
+    </BrowserRouter>
 )
